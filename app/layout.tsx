63e84ca44a1ff36fa152b99a7afc33bf3eca64b9
torobucci <kevintoro254@gmail.com>
2024-08-09 13:08:38 +0300

    update error in modifying favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,6 @@ export default async function RootLayout({
   const session = await auth()
   return (
     <html lang="en">
-      <head>
-        <title>Shopco</title>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-
-      </head>
       <body className={`${inter.className}`}>
         <StoreProvider>
           <SessionProviderClientComponent session={session}>
