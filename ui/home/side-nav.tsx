6fa8c260a6215b12318462abb606958f84d6406c
torobucci <kevintoro254@gmail.com>
2024-08-18 23:30:38 +0300

    fix typescript error

--- ui/home/side-nav.tsx
+++ ui/home/side-nav.tsx
@@ -3,7 +3,7 @@ import { BiCategory } from "react-icons/bi";
 import { SignOut } from "../signout";
 import { SideNavLinks } from './sidenav_links'
 
-export function SideNav({ categories }: { categories: Categories[] }) {
+export function SideNav({ categories }: { categories: Categories[] | undefined }) {
 
     return (
         <div className="hidden lg:flex lg:max-w-[250px] flex-col justify-between flex-[0.55] mr-6 p-4 border-[1px] border-gray-300 text-black text-opacity-60 text-base rounded-md min-w-[250px] h-screen overflow-y-auto">
--- ui/home/sidenav_links.tsx
+++ ui/home/sidenav_links.tsx
@@ -4,11 +4,11 @@ import { Categories } from "../../lib/definitions";
 import { FaAngleRight } from "react-icons/fa6";
 import Link from "next/link"
 import { usePathname } from "next/navigation";
-export function SideNavLinks({ categories }: { categories: Categories[] }) {
+export function SideNavLinks({ categories }: { categories: Categories[] | undefined }) {
     const currentPath = usePathname().split('/')
     return (
         <div>
-            {categories.map((category, index) => {
+            {categories?.map((category, index) => {
                 return (
                     <Link href={`/home/categories/${category.name}`} className={`${currentPath[currentPath.length - 1] == category.name ? 'bg-slate-200' : ''} flex items-center p-2 mb-[2px] rounded-md justify-between hover:bg-slate-200 text-black text-opacity-60`} key={index}>
                         <p>{category.name}</p>
