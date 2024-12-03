f1688fa8d77f76ec57c994fa75e6b8566b96a3f0
torobucci <kevintoro254@gmail.com>
2024-12-03 16:57:46 +0300

    add /product in (main) route to display product item

---
+++ app/(main)/[product]/page.tsx
@@ -0,0 +1,15 @@
+import LinkHistory from "@ui/home/nav-links"
+import { fetchProductByName } from "@lib/data"
+import ProductDetails from "@ui/home/product-details"
+import { auth } from "../../../auth"
+import { Session } from "inspector"
+export default async function page({ params }: { params:{productname: number} }) {
+   const productId = params.productname
+    const product = await fetchProductByName(productId)
+    const session  = await auth()
+
+    return <div className="mx-4 xsm:mx-6 md:mx-8 lg:mx-12 pt-[74px] pb-28">
+        <LinkHistory />
+        <ProductDetails product={product} session={session}/>
+    </div>
+}
